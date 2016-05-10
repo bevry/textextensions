113b61902de14c8763c033748a52c7fe56ce8f03
Benjamin Lupton <b@lupton.cc>
2016-05-10 12:24:32 +0800

    v2.0.0. Now a json file.

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,8 @@
 # History
 
+## v2.0.0 2016 May 10
+- Now a json file
+
 ## v1.1.0 2016 May 10
 - Added about 150 additional text extensions
 	- Thanks to [FelipeBB](https://github.com/FelipeBB) for [pull request #3](https://github.com/bevry/textextensions/pull/3)
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 var fs = require('fs')
-var equal = require('assert-helpers').equal
-var sourcePath = require('path').join(__dirname, 'index.js')
+var assert = require('assert-helpers')
+var sourcePath = require('path').join(__dirname, 'index.json')
 var suite = require('joe').suite
+var indentation = '\t'
 
 suite('textextensions', function (suite, test) {
 	var sourceContent, sourceData;
@@ -9,52 +10,55 @@ suite('textextensions', function (suite, test) {
 	test('read the file', function (next) {
 		fs.readFile(sourcePath, function (error, data) {
 			if ( error )  return next(error)
-			sourceContent = data.toString()
+			sourceContent = data.toString().trim()
 			next()
 		})
 	})
 
-	test('uses tabs', function () {
-		if ( sourceContent.indexOf('\t') !== -1 && sourceContent.indexOf('  ') === -1 ) {
-			// all good
-		}
-		else {
-			return new Error('tabs have not been used')
-		}
-	})
-
-	test('no semicolon', function () {
-		if ( sourceContent.indexOf(';') !== -1 ) {
-			return new Error('semicolon was used')
-		}
-	})
-
-	test('parse the file', function () {
+	test('require the file', function () {
 		sourceData = require(sourcePath)
 	})
 
-	test('data was sorted', function () {
-		var actual = sourceData
-		var expected = sourceData.slice().sort()
-		equal(
-			JSON.stringify(actual, null, '\t'),
-			JSON.stringify(expected, null, '\t')
-		)
+	test('parse the file', function () {
+		sourceData = JSON.parse(sourceContent)
+	})
+
+	test('data had correct format', function (next) {
+		var expected = JSON.stringify(sourceData, null, indentation)
+		try {
+			assert.equal(
+				sourceContent,
+				expected
+			)
+		}
+		catch ( err ) {
+			console.log('format was not correct, fixing...')
+			return fs.writeFile(sourcePath, expected, function (error) {
+				if ( error )  return next(error)
+				sourceContent = expected
+				console.log('fixed indentation for next commit')
+				return next(err)
+			})
+		}
+		return next()
 	})
 
 	test('data had duplicates removed', function () {
 		var map = {}
-		var actual = sourceData
-		var expected = sourceData.filter(function (i) {
+		sourceData.filter(function (i) {
 			if ( typeof map[i] === 'undefined' ) {
 				map[i] = true
 				return true
 			}
-			return false
+			throw new Error(i + ' is duplicated')
 		})
-		equal(
-			JSON.stringify(actual, null, '\t'),
-			JSON.stringify(expected, null, '\t')
+	})
+
+	test('data was sorted', function () {
+		var expected = sourceData.slice().sort()
+		assert.equal(
+			sourceContent,
+			JSON.stringify(expected, null, indentation)
 		)
 	})
 })
